e3a287282f2369620ab9d37674bfb18501ffe5ff
vitalii-speka <65010140+vitalii-speka@users.noreply.github.com>
2021-03-20 17:32:39 +0200

    save, one component useMenu

--- src/componets/UserMenu/UserMenu.js
+++ src/componets/UserMenu/UserMenu.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Button from 'react-bootstrap/Button';
 import { getUserName, logOut } from '../../redux/auth';
 
@@ -7,23 +7,21 @@ import './UserMenu.css';
 
 import defaultAvatar from './default-user.png';
 
-const UserMenu = ({ avatar, name, onLoguout }) => (
-  <div className="container-user">
-    <img src={avatar} alt="" width="32" className="avatar" />
-    <span className="name">Welcome, {name}</span>
-    <Button variant="outline-info" onClick={onLoguout}>
-      Logout
-    </Button>
-  </div>
-);
+export default function UserMenu() {
+  const dispatch = useDispatch();
+  const name = useSelector(getUserName);
 
-const mapStateToProps = state => ({
-  avatar: defaultAvatar,
-  name: getUserName(state),
-});
+  const onLogOut = useCallback(() => {
+    dispatch(logOut());
+  }, [dispatch]);
 
-const mapDispatchToProps = {
-  onLoguout: logOut,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(UserMenu);
+  return (
+    <div className="container-user">
+      <img src={defaultAvatar} alt="" width="32" className="avatar" />
+      <span className="name">Welcome, {name}</span>
+      <Button variant="outline-info" onClick={onLogOut}>
+        Logout
+      </Button>
+    </div>
+  );
+}
